6e3e697e2be344afa1dca302135e6d1e45ec66dd
Nhat Nguyen <phongnhat19@gmail.com>
2019-03-07 14:49:19 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    query: {
+                        plugins: ['transform-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
