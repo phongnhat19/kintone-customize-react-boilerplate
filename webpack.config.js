ea10fd036de4efedf34b7c0473c324a7ce1c228d
Nguyen Phong Nhat <phongnhat19@gmail.com>
2019-03-12 09:54:47 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./public/index.html",
+    filename: "./index.html"
+});
 module.exports = {
     module: {
         rules: [
@@ -13,22 +19,9 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true
-                        }
-                    }
-                ]
+                use: ["style-loader", "css-loader"]
             }
         ]
-    }
+    },
+    plugins: [htmlPlugin]
 };
